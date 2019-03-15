079ec607810934fbebf0819cf3e730e292350334
Илья Веселов <ves.ilya@mail.ru>
2019-03-15 22:10:31 +0300

    Добавление проекта

---
+++ BookLib/BookLib/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookLib.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
