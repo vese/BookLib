df87759e03df767b0865e7b31837beb94d194256
Илья Веселов <ves.ilya@mail.ru>
2019-03-18 23:36:40 +0300

    Добавлен консольный проект для тестирования библиотеки классов

---
+++ BookLib/BookLib.Cons/Program.cs
@@ -0,0 +1,20 @@
+﻿using BookLib.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace BookLib.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseLazyLoadingProxies()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-BookLib-4E6ABAD7-8749-4CE7-B8FE-3C6CFCF09A4C;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+            using (ApplicationDbContext context = new ApplicationDbContext(options))
+            {
+                context.Book.ToList().ForEach(b => Console.WriteLine(b.Name));
+            }
+        }
+    }
+}
