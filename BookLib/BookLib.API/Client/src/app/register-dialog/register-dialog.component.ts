20b65de06cdf72456c72e7cb3a402ec05c254b3b
Илья Веселов <ves.ilya@mail.ru>
2019-04-08 16:11:57 +0300

    Angular: валидация при регистрации

--- BookLib/BookLib.API/Client/src/app/register-dialog/register-dialog.component.ts
+++ BookLib/BookLib.API/Client/src/app/register-dialog/register-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { UserService, UserDialogData } from '../user.service';
+import { FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-dialog',
@@ -14,6 +15,37 @@ export class RegisterDialogComponent {
   loggedIn: boolean = false;
   registerFailed: boolean;
   failtureMessage: string;
+
+  loginFormControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(20)
+  ]);
+  passwordFormControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  passwordConfirmFormControl = new FormControl("", [
+    Validators.required
+  ]);
+
+  getLoginErrorMessage() {
+    return this.loginFormControl.hasError('required') ? 'Введите значение' :
+      this.loginFormControl.hasError('minlength') ? 'Длина поля должна быть минимум 4 символов' :
+        this.loginFormControl.hasError('maxlength') ? 'Длина поля должна быть максимум 20 символов' :
+          '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.passwordFormControl.hasError('required') ? 'Введите значение' :
+      this.passwordFormControl.hasError('minlength') ? 'Длина поля должна быть минимум 6 символов' :
+        '';
+  }
+
+  getPasswordConfirmErrorMessage() {
+    return this.passwordConfirmFormControl.hasError('required') ? 'Введите значение' :
+      '';
+  }
 
   constructor(public dialogRef: MatDialogRef<RegisterDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: UserDialogData,
