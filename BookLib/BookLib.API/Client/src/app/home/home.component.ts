c5b4e7609803d09e5395725338bd8ec48d9008ba
Перов Михаил <pepperov@ya.ru>
2019-04-15 00:18:47 +0300

    Верстка, CSS и оформление books, book-detail.

--- BookLib/BookLib.API/Client/src/app/home/home.component.ts
+++ BookLib/BookLib.API/Client/src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit
 {
-  title = 'BookLib';
+  title = 'Добро пожаловать в библиотеку!';
 
   constructor() { }
 
