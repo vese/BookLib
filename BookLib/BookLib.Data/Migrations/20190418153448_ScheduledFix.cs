3f844870e4b64330e3fa28b9203ffa7d420f4570
Илья Веселов <ves.ilya@mail.ru>
2019-04-18 18:40:20 +0300

    Миграция - переименование в SheduledFix

---
+++ BookLib/BookLib.Data/Migrations/20190418153448_ScheduledFix.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookLib.Data.Migrations
+{
+    public partial class ScheduledFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "SheduledBook",
+                newName: "ScheduledBook");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "ScheduledBook",
+                newName: "SheduledBook");
+        }
+    }
+}
